65db7bc2e6913007c78eafba82f1141b69c10700
jasoneliann <jasoneliann@gmail.com>
2016-11-14 14:22:46 +0700

    upload foto

--- vendors/js/main.js
+++ vendors/js/main.js
@@ -278,3 +278,13 @@ $('#modalRating').on('show.bs.modal', function (event) {
   modal.find('.modal-title').text('New message to ' + recipient);
   modal.find('.modal-body input').val(recipient);*/
 });
+
+$('#modalUpload').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var recipient = button.data('menu'); // Extract info from data-* attributes
+	var restoran = button.data('restoran');
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this);
+  modal.find('.modal-title').text("Upload Foto " + recipient + " Restoran "+ restoran);
+});
